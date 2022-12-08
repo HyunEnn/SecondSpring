428470a247cef30d3d921dd445d2f81421858f8f
HYUNJUN, KIM <denny10002@naver.com>
2022-12-08 23:19:42 +0900

    Spring Basic 5th day

--- src/main/java/SecondSpring/core/AppConfig.java
+++ src/main/java/SecondSpring/core/AppConfig.java
@@ -22,12 +22,12 @@ public class AppConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
     @Bean
     public OrderService orderService() {
-
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), getDiscountPolicy());
     }
 
---
+++ src/test/java/SecondSpring/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,40 @@
+package SecondSpring.core.singleton;
+
+import SecondSpring.core.AppConfig;
+import SecondSpring.core.member.MemberRepository;
+import SecondSpring.core.member.MemberServiceImpl;
+import SecondSpring.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configuarationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberService -> memberRepository = " + memberRepository1);
+        System.out.println("orderService -> memberRepository = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+
+        Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+    }
+}
