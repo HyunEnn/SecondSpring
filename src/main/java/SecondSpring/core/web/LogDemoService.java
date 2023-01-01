ea278bad551485c91d3d7a2c3a33081d8f1c59c5
HYUNJUN, KIM <denny10002@naver.com>
2023-01-01 23:44:45 +0900

    Basic Spring section 9 study

--- src/main/java/SecondSpring/core/web/LogDemoService.java
+++ src/main/java/SecondSpring/core/web/LogDemoService.java
@@ -3,16 +3,20 @@ package SecondSpring.core.web;
 import SecondSpring.core.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Provider;
 
 @Service
 @RequiredArgsConstructor // 자동으로 생성자 만들어줌
 public class LogDemoService {
 
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
     public void logic(String id) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + id);
     }
 }
